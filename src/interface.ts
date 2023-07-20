d4925475b32071ab6f8af91b287c1a953954e34e
Olga_Belaya <protenot@yandex.ru>
2023-07-20 14:35:09 +0300

    create create todotask

--- src/interface.ts
+++ src/interface.ts
@@ -1,30 +1,60 @@
-enum Status {
-  "delayed",
-  "done",
-  "pending",
-  "cancelled",
+export enum Status {
+  Delayed = "delayed",
+  Done = "done",
+  Pending = "pending",
+  Cancelled = "cancelled",
 }
-interface ToDoTask {
+export interface ToDoTask {
   id: number;
   date: Date;
   content: string;
   status: Status;
 }
 
-interface Filter {
+export interface Filter {
   date: Date;
   content: string;
   status: Status;
 }
 
-interface ToDoTaskLibrary {
-  tasks: ToDoTask[];
-  createToDoTask(task: ToDoTask): Promise<string>;
-  getToDoTask(filter: Filter): Promise<Partial<ToDoTask>>;
+export interface ToDoTaskLibrary {
+  //tasks: ToDoTask[];
+  createToDoTask(text?: string, task?: ToDoTask): Promise<string>;
+  getToDoTask(task: ToDoTask): Promise<Partial<ToDoTask>>;
   getAll(): Promise<ToDoTask[]>;
   updateToDoTask(
     id: number,
     updateToDoTask: Partial<ToDoTask>,
   ): Promise<Partial<ToDoTask>>;
   deleteToDoTask(id: number): Promise<void>;
+  filterDates(date: Date): Promise<ToDoTask | ToDoTask[] | undefined>;
+  filterContent(content: string): Promise<ToDoTask | ToDoTask[] | undefined>;
+  filterStatus(status: Status): Promise<ToDoTask | ToDoTask[] | undefined>;
+}
+export function createID(): number {
+  return Math.floor(Math.random() * 1000);
+}
+export class ToDoList {
+  tasks: ToDoTask[];
+  constructor() {
+    this.tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+  }
+  async createToDoTask(text: string, task?: ToDoTask): Promise<string> {
+    return new Promise((resolve, reject) => {
+      if (this.tasks.some((t) => t.content == task.content)) {
+        reject("Такая задача уже существует");
+      } else {
+        const newToDoTask = {
+          ...task,
+          id: createID(),
+          date: new Date(),
+          content: text,
+          status: Status.Pending,
+        };
+        this.tasks.push(newToDoTask);
+        localStorage.setItem("tasks", JSON.stringify(this.tasks));
+        resolve("Задача создана");
+      }
+    });
+  }
 }
